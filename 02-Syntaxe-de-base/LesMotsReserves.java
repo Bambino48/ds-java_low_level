b90d4a7b122390b4aa46aefffea97e967a421739
Bambino48 <ibamba366@gmail.com>
2024-04-29 12:11:37 +0000

    les mots reservés en java

--- 02-Syntaxe-de-base/LesMotsReserves.java
+++ 02-Syntaxe-de-base/LesMotsReserves.java
@@ -1,4 +1,133 @@
 public class LesMotsReserves {
-    
+
+    // Dans cette classe nous allons faire la liste des mots clés réservés en java
+    // et leurs explications
+
+    /*
+     * Abstract : Selon le dictionnaire, l'abstraction est la qualité de traiter des
+     * idées plutôt que des événements.
+     * 
+     * Assert : Le mot-clé assert est utilisé en Java pour définir l'assertion.
+     * Une assertion est une instruction en Java qui garantit l'exactitude de toutes
+     * les hypothèses formulées dans le programme.
+     * 
+     * Boolean : Le type de données booléen est l'un des huit types de données
+     * primitifs pris en charge par Java.
+     * Il fournit des moyens de créer des variables de type booléen qui peuvent
+     * accepter une valeur booléenne comme vraie ou fausse.
+     * 
+     * Break : L' instruction break dans le langage de programmation Java a les deux
+     * utilisations suivantes :
+     * Lorsque l' instruction break est rencontrée à l'intérieur d'une boucle,
+     * la boucle est immédiatement terminée et le contrôle du programme reprend à
+     * l'instruction suivante suivant la boucle.
+     * Il peut être utilisé pour terminer un cas dans l' instruction switch .
+     * 
+     * Byte : Le type de données octet est l'un des huit types de données primitifs
+     * pris en charge par Java.
+     * Il fournit des moyens de créer des variables de type octet pouvant accepter
+     * une valeur d'octet.
+     * 
+     * Case : Le mot-clé case fait partie de l'instruction switch qui permet de
+     * tester l'égalité d'une variable par rapport à une liste de valeurs.
+     * 
+     * Catch :Une exception (ou événement exceptionnel) est un problème qui survient
+     * lors de l'exécution d'un programme.
+     * 
+     * Char : Le type de données char est l'un des huit types de données primitifs
+     * pris en charge par Java.
+     * 
+     * Class : Java est un langage orienté objet. En tant que langage doté de la
+     * fonctionnalité orienté objet.
+     * 
+     * Const : Le mot-clé final est utilisé pour définir une valeur constante ou des
+     * méthodes/classes finales en Java.
+     * 
+     * Continue : Le mot-clé continue peut être utilisé dans n’importe quelle
+     * structure de contrôle de boucle.
+     * 
+     * Default : Le mot-clé default fait partie de l'instruction switch qui permet
+     * de tester l'égalité
+     * d'une variable par rapport à une liste de valeurs.
+     * 
+     * Do : Une boucle do...while est similaire à une boucle while, sauf qu'une
+     * boucle do...while est garantie de s'exécuter au moins une fois.
+     * 
+     * Double : Le type de données double est l'un des huit types de données
+     * primitifs pris en charge par Java.
+     * 
+     * If : Le type de données double est l'un des huit types de données primitifs
+     * pris en charge par Java.
+     * 
+     * Enum : La classe Java Enum est la classe de base commune à tous les types
+     * d'énumération du langage Java.
+     * 
+     * Extends : extends est le mot-clé utilisé pour hériter des propriétés d'une
+     * classe. Voici la syntaxe du mot-clé extends.
+     * 
+     * Final : Le mot-clé final est utilisé pour définir une valeur constante ou des
+     * méthodes/classes finales en Java.
+     * 
+     * Fanilly : Le mot-clé enfin est utilisé pour définir un bloc final. Le bloc
+     * final suit un bloc try ou un bloc catch.
+     * Un bloc de code final s'exécute toujours, quelle que soit l'occurrence d'une
+     * exception.
+     * 
+     * Float : Le type de données float est l'un des huit types de données primitifs
+     * pris en charge par Java.
+     * Il fournit des moyens de créer des variables de type float pouvant accepter
+     * une valeur float.
+     * 
+     * For : Une boucle for est une structure de contrôle de répétition qui vous
+     * permet d'écrire efficacement
+     * une boucle qui doit être exécutée un nombre spécifique de fois.
+     * 
+     * Goto : L'instruction goto n'est actuellement pas prise en charge par Java. Il
+     * est conservé comme mot-clé réservé pour le futur.
+     * Comme alternative, Java prend en charge les étiquettes avec les instructions
+     * break et continue.
+     * 
+     * If : Une instruction if consiste en une expression booléenne suivie d'une ou
+     * plusieurs instructions.
+     * 
+     * Implements : Généralement, le mot-clé Implements est utilisé avec des classes
+     * pour hériter des propriétés d'une interface.
+     * 
+     * Import : Le clavier d'importation est utilisé dans le contexte des packages.
+     * 
+     * Instanceof : Le mot-clé instanceof est un opérateur utilisé uniquement pour
+     * les variables de référence d'objet.
+     * 
+     * Int : Le type de données int est l'un des huit types de données primitifs
+     * pris en charge par Java.
+     * 
+     * Interface : Le type de données int est l'un des huit types de données
+     * primitifs pris en charge par Java.
+     * 
+     * Long : Le type de données long est l'un des huit types de données primitifs
+     * pris en charge par Java.
+     * 
+     * Native
+     * 
+     * New
+     * 
+     * package :
+     * private :
+     * protected :
+     * public :
+     * Return
+     * Short :
+     * Static :
+     * Strictfp
+     * Super :
+     * Switch :
+     * This :
+     * Throw :
+     * Transient :
+     * Try :
+     * Void :
+     * Volatile :
+     * While :
+     */
 
 }
